b482756811b534e43206eeffbeeabb80853c599f
Sora <brenoolieveira1611@gmail.com>
2025-03-18 22:05:20 -0300

    > Aprendizado 18/03

--- poo/obj.js
+++ poo/obj.js
@@ -1,21 +1,44 @@
-class LoL {
-    constructor(champ, maestria, classe, origem, feedado, farm, kill, morte){
-        this.champ = champ
-        this.maestria = maestria
-        this.classe = classe
-        this.origem = origem
-        this.feedado = feedado
-        this.farm = farm
-        this.kill = kill
-        this.morte = morte
+class Pessoa {
+    constructor(pnome, pidade, pcor, paltura, pualidade, pdefeito, pstatus, pestafa){
+        this.nome = pnome
+        this.idade = pidade
+        this.cor = pcor
+        this.altura = paltura
+        this.qualidade = pualidade
+        this.defeito = pdefeito
+        this.estafa = 0
+
+        if(pstatus == 1){
+            this.status = "Acordado"
+        }else{
+            this.status = "Dormindo"
+        }
+    }
+    
+    acordar(){
+        this.status == 1
     }
 
+    setnome(Novonome){
+        this.nome = Novonome
+    }
+
+    info(){
+        console.log(`Nome: ${this.nome}`)
+        console.log(`Idade: ${this.idade}`)
+        console.log(`Cor: ${this.cor}`)
+        console.log(`Altura: ${this.altura}`)
+        console.log(`Qualidade: ${this.qualidade}`)
+        console.log(`Defeito: ${this.defeito}`)
+        console.log(`Status: ${this.status}`)
+        console.log(`Estafa: ${this.estafa}`)
+        console.log("-------------------------------")
+    }
 }
 
-let Katarina = new LoL("Katarina", 549.2, "assassino", "Noxus")
-let Fizz = new LoL("Fizz", 1.923 , "assassino", "Bandopolis")
-let Irelia = new LoL("Irelia", 1.002, "lutador", "Demacian")
-let Teemo = new LoL("Teemo", 42.50, "tank", "Bandopolis")
-let Diana = new LoL("Diana", 842.22, "lutador", "Shurima")
+let p1 = new Pessoa("Breno", 19, "Branco", 1.81, "Ser Legal", "Ser Chato", 1)
+let p2 = new Pessoa("Heloisa", 18, "Branco", 1.65, "Ser Amorosa", "Ser Tenebrosa", 1)
+let p3 = new Pessoa("Mark", 19, "Pardo", 1.64, "Ser Inteligente", "Ser Burro", 1)
+let p4 = new Pessoa("Yuki", 19, "Amarelo", 1.68, "Ser Interessante", "Ser Desinteressante", 2)
 
-console.log(Fizz, Irelia, Diana)
+p1.info()
