483ff627fdcfa717e925263bdbab452eabb22108
Sora <brenoolieveira1611@gmail.com>
2025-04-24 22:42:43 -0300

    > Aprendizado 24/04

--- poo/obj.js
+++ poo/obj.js
@@ -1,28 +1,6 @@
-function Pessoa(nome,sobrenome,idade){
-  this.nome = nome;
-  this.sobrenome = sobrenome;
-  this.idade = idade;
-  this.amigos = [];
-
-  this.nomecompleto = () => {
-    return `${this.nome} ${this.sobrenome}`;
-  };
-
-  this.adicionaramigo = (amigo) => {
-    this.amigos.push(amigo);
-    amigo.amigos.push(this)
-  };
-
-  this.selecionarAmigos = () => {
-    return this.amigos.map((amigo) => amigo.nomecompleto())
-  };
+class Pessoa {
+  constructor(nome,sobrenome,idade){
+    this.nome = nome
+    this.sobre
+  }
 }
-
-const pessoa1 = new Pessoa("Breno", "Oliveira", 19);
-const pessoa2 = new Pessoa("Heloisa", "Anjos", 18);
-const pessoa3 = new Pessoa("Felipe", "Gonçalve", 18);
-
-pessoa1.adicionaramigo(pessoa2)
-pessoa1.adicionaramigo(pessoa3)
-console.log(pessoa1.selecionarAmigos());
-console.log(pessoa2.selecionarAmigos());
