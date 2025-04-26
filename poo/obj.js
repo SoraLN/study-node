f2d4ae32f12c52cebf036cfc299194e6fff98f7c
Sora <brenoolieveira1611@gmail.com>
2025-04-26 12:21:50 -0300

    > Aprendizado 26/04

--- poo/obj.js
+++ poo/obj.js
@@ -1,6 +1,33 @@
-class Pessoa {
-  constructor(nome,sobrenome,idade){
-    this.nome = nome
-    this.sobre
+function novoUsuario(nome, idade, email){
+
+  const id = {
+    nome: nome,
+    idade: idade,
+    email: email
   }
+
+  if(typeof nome === "string" && typeof idade === "number"){
+
+    return id
+
+  }else{ 
+
+    return `O usuario ${nome} esta com alguma informação incorreta, digite o nome ou idade corretamente`
+
+  }
+
 }
+
+const total = [novoUsuario("Breno", 19, "teste.teste.com"), novoUsuario("Heloisa", 18, "helo@teste.com")]
+
+const total2 = [...total, novoUsuario("Davi", 20, "sla#gmail.com")]
+
+
+console.log(total2)
+
+
+// VC VAI CRIAR UMA FUNCAO QUE RECEBA UM Object, ok
+// JOGUE ELE PARA UM Array, ok
+// TRASFORME ELE EM STRING ok
+// GARANTA QUE O NOME É UMA STRING E IDADE UM NUMBER ok
+// ULTILIZE SPREAD PARA ADD NOVOS USUARIOS
